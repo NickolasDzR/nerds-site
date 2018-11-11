ba77bbb9542f9d60b29936e1b5b1bfe99c43c0f5
NickolasDzR <42074447+NickolasDzR@users.noreply.github.com>
2018-11-11 21:00:03 +0300

    popup is ready

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-
 var link = document.querySelector(".login-link");
 var popup = document.querySelector('.modal-unshow');
 var close = document.querySelector('.modal-close');
-
+var text = popup.querySelector('[name=your-name]');
+var form = popup.querySelector('form');
 link.addEventListener('click', function (evt) {
   evt.preventDefault();
-popup.classList.add('modal-show');
+  popup.classList.add('modal-show');
+  text.focus();
 });
 close.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.remove('modal-show');
 });
-
-<link rel='stylesheet' href='js/js.js' type='text/js' />
