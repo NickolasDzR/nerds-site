4964b48c7713d5296f5ccb0c6ece0dfdd9ca6af5
NickolasDzR <42074447+NickolasDzR@users.noreply.github.com>
2018-11-13 07:36:24 +0300

    коммит карты

--- js/script.js
+++ js/script.js
@@ -19,15 +19,11 @@ container-map
 */
 
 function initMap() {
-	// Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
 	var centerLatLng = new google.maps.LatLng(59.938631, 30.323055);
-	// Обязательные опции с которыми будет проинициализированна карта
 	var mapOptions = {
-		center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
-		zoom: 8               // Зум по умолчанию. Возможные значения от 0 до 21
+		center: centerLatLng,
+		zoom: 15
 	};
-	// Создаем карту внутри элемента #map
-	var map = new google.maps.Map(document.getElementById('container-map'), mapOptions);
+	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
-// Ждем полной загрузки страницы, после этого запускаем initMap()
 google.maps.event.addDomListener(window, "load", initMap);
